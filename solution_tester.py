fd56dd90b77b949d3e9485ec5640551f4c49f735
Deniz Lapsekili <71494496+3arii@users.noreply.github.com>
2022-04-15 08:13:05 +0300

    Added muon test, fixed mathematical errors

--- solution_tester.py
+++ solution_tester.py
@@ -10,15 +10,16 @@ br_values = [63.56/100, 5.07/100, 3.352/100, 20.67/100, 1.76/100, 5.583/100, 6.2
 a, b, c, d, e, f, g, h, i, j = symbols("a, b, c, d, e, f, g, h, i, j")
 
 velocity = int(input("Please enter the velocity value: "))
-electron_n = int(input("Please enter the electron number: "))
+positron_n = int(input("Please enter the electron number: "))
 kplus_n = int(input("Please enter the Kaon+ number: "))
 piplus_n = int(input("Please enter the π+ number: "))
 pinegative_n = int(input("Please enter the π- number: "))
+muon_n = int(input("Please enter the muon number: "))
 
-gamma = (1/sqrt(1 - velocity**2 / 299792458**2))
+gamma = (sqrt(1 - velocity**2 / 299792458**2))
 
 sol1 = ((gamma) * (1.238*10**8))
-eq1 = Eq((a*(br_values[0]) + b*(br_values[1]) + c*(br_values[2]) + d*(br_values[3]) + e*(br_values[4]) + f*(br_values[5]) + g*(br_values[6]) + h*(br_values[7]) + i*(br_values[8]) + j*(br_values[9]) ), sol1)
+eq1 = Eq((a*7.869*10**7 + b*6.277*10**6 + c*4.129*10**6 + d*2.559*10**7 + e*2.179*10**6 + f*2.179*10**6), sol1)
 
 
 for i in br_values:
@@ -26,12 +27,17 @@ for i in br_values:
   eq_library.append(eq1)
 
 
-solution_dictionary = solve((eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0]), (a, b, c, d, e, f, g, h, i, j))
+solution_dictionary = solve((eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0], eq_library[0]), (a, b, c, d, e, f, g))
 
-# Electron number test
-eq1 = Eq((kplus_n * (solution_dictionary[1] * solution_dictionary[9])), electron_n)
+# Positron number test
+eq1 = Eq((kplus_n * (solution_dictionary[1] * solution_dictionary[9])), positron_n)
 if(solve(eq1)):
   tested_solution_dictionary.append(solution_dictionary[1], solution_dictionary[9])
+  
+# Muon number test
+eq1 = Eq((kplus_n *(solution_dictionary[0]*0.6349 + solution_dictionary[2]*0.3333 + solution_dictionary[6]*0.0062) + solution_dictionary[7]*0.004 + solution_dictionary[8]*0.0033), muon_n)
+if(solve(eq1)):
+  tested_solution_dictionary.append(solution_dictionary[0], solution_dictionary[2], solution_dictionary[6], solution_dictionary[7], solution_dictionary[8])
 
 # π+ number test
 eq1 = Eq((kplus_n * (solution_dictionary[3] + solution_dictionary[4] + 2*solution_dictionary[5])) , piplus_n)
